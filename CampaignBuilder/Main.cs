016bb8ad8234d75639e7f61f3fd8ee0bcc19bb54
Ben Hall <bhall@tampaprep.org>
2017-11-03 10:06:14 -0400

    Added mac-os project template

---
+++ CampaignBuilder/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace CampaignBuilder
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
