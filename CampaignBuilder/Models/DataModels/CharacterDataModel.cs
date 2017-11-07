24b1e1a0c58fe6d15d82afb6f04d05b1e9a26858
Ben Hall <bhall@tampaprep.org>
2017-11-07 12:47:57 -0500

    Added data model for character

---
+++ CampaignBuilder/Models/DataModels/CharacterDataModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CampaignCore;
+
+namespace CampaignBuilder.Models.DataModels
+{
+    public class CharacterDataModel
+    {
+        public CharacterDataModel(Character character, CharacterType role)
+        {
+            mCharacter = character;
+            mRole = role;
+        }
+
+        public string Name
+        {
+            get { return mCharacter.Name; }
+            set { mCharacter.Name = value; }
+        }
+
+        private Character mCharacter;
+        private CharacterType mRole;
+    }
+}
